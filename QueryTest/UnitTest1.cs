5323331fc57b59b6351eb9bd36782a436c282cae
Furesoft <filmee24@gmail.com>
2020-01-18 14:33:49 +0100

    added test project

---
+++ QueryTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Furesoft.Proxy.Query;
+using NUnit.Framework;
+
+namespace QueryTest
+{
+    public class Tests
+    {
+        [Test]
+        public void ContentParseTest()
+        {
+            var result = QueryEvaluator.ParseQuery("content on \"mysite.domain\" display \"<h1> my own site</h1>\"");
+            result?.DoBlock(null);
+
+            Assert.Pass();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
